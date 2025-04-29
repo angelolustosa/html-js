772248ef78f68ad01e4b8d5222efa4e70ac918d5
Angelo Lustosa de Figueiredo Filho <Angelo.Filho@digitalcollege.internal>
2025-04-29 20:26:06 -0300

    Variaveis e tipos de dados

---
+++ conceito_variavel.js
@@ -0,0 +1,12 @@
+//criação de variável (const, let, var)
+const n1 = 10;
+const n2 = 40;
+
+//soma colocando o valor da soma em uma variável
+const resultado = n1 + n2;
+
+
+//exibir o resultado da soma
+//console.log(resultado);
+
+
---
+++ tipos_dados.js
@@ -0,0 +1,28 @@
+//var, const e let
+
+//1 - const (constante)
+const pi = 3.14;
+//imprimindo a variável no console
+//console.log(pi);
+
+//2 - let (o valor da variável pode ser alterado)
+let nome = 'Angelo'
+
+let nome_atualizado = 'Angelo Lindão!';
+console.log(nome);
+
+let desconto = 20.5;
+
+// atulizando o valor da variavel utilizando a própria variável
+desconto = desconto * 2;
+console.log('Desconto atualizado: ' + desconto);
+
+//Array (lista)
+let tamanhos = ['P', 'PP', 'M', 'G', 36, 38, 40, 54];
+
+let temPet = false;
+
+const idade = 28;
+
+let dataCadastro = new Date(); //pega a data e hora atual
+console.log(dataCadastro);
